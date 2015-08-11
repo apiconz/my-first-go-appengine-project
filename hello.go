26778e3253e0eb1ca9a064d26395eba496982aca
Armando-Satellite <apiconz@gmail.com>
2015-08-11 02:07:46 -0500

    Using the datastore

--- hello.go
+++ hello.go
@@ -2,35 +2,75 @@ package hello
 
 import (
 	"html/template"
-	"io/ioutil"
 	"net/http"
+	"time"
+
+	"appengine"
+	"appengine/datastore"
+	"appengine/user"
 )
 
 var (
-	guestbookForm []byte
-	signTemplate  = template.Must(template.ParseFiles("guestbook.html"))
+	guestbookTemplate = template.Must(template.ParseFiles("guestbook.html"))
 )
+
+type Greeting struct {
+	Author  string
+	Content string
+	Date    time.Time
+}
 
 func init() {
 	// Leemos el formulario
-	content, err := ioutil.ReadFile("guestbookform.html")
+	//content, err := ioutil.ReadFile("guestbookform.html")
 	// Evaluamos el error
-	if err != nil {
-		panic(err)
-	}
-	guestbookForm = content
+	//if err != nil {
+	//	panic(err)
+	//}
+	//guestbookForm = content
 
 	http.HandleFunc("/", root)
 	http.HandleFunc("/sign", sign)
 }
 
+func guestbookKey(c appengine.Context) *datastore.Key {
+	return datastore.NewKey(c, "Guestbook", "default_guestbook", 0, nil)
+}
+
 func root(w http.ResponseWriter, r *http.Request) {
-	w.Write(guestbookForm)
+	c := appengine.NewContext(r)
+
+	q := datastore.NewQuery("Greeting").Ancestor(guestbookKey(c)).Order("-Date").Limit(10)
+	greetings := make([]Greeting, 0, 10)
+	if _, err := q.GetAll(c, &greetings); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := guestbookTemplate.Execute(w, greetings); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func sign(w http.ResponseWriter, r *http.Request) {
-	err := signTemplate.Execute(w, r.FormValue("content"))
+	c := appengine.NewContext(r)
+	g := Greeting{
+		Content: r.FormValue("content"),
+		Date:    time.Now(),
+	}
+
+	if u := user.Current(c); u != nil {
+		g.Author = u.String()
+	}
+
+	key := datastore.NewIncompleteKey(c, "Greeting", guestbookKey(c))
+	_, err := datastore.Put(c, key, &g)
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
+
+	http.Redirect(w, r, "/", http.StatusFound)
+
 }
