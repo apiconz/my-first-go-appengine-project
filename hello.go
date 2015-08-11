b057f7eb97a841d725097acabeae18168ce513ff
Armando-Satellite <apiconz@gmail.com>
2015-08-10 23:07:02 -0500

    Handling Forms

--- hello.go
+++ hello.go
@@ -2,34 +2,48 @@ package hello
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
-
-	"appengine"
-	"appengine/user"
 )
 
 func init() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", root)
+	http.HandleFunc("/sign", sign)
 }
 
-func handler(rw http.ResponseWriter, req *http.Request) {
-	// Crea un nuevo contexto
-	c := appengine.NewContext(req)
-	// Obtiene el usuario actual
-	u := user.Current(c)
-	// Si no hay usuario (o sea no se ha logueado)
-	if u == nil {
-		// Obtenemos la url de logueo
-		url, err := user.LoginURL(c, req.URL.String())
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		// Mostramos la url en pantalla
-		rw.Header().Set("Location", url)
-		rw.WriteHeader(http.StatusFound)
-		return
-	}
-	//
-	fmt.Fprintf(rw, "Hello, %v!", u)
+func root(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, guestbookForm)
 }
+
+const guestbookForm = `
+	<html>
+		<body>
+			<form action="/sign" method="post">
+				<div>
+					<textarea name="content" rows="3" cols="60"></textarea>
+				</div>
+				<div>
+					<input type="submit" value="Sign Guestbook">
+				</div>
+			</form>
+		</body>
+	</html>
+`
+
+func sign(w http.ResponseWriter, r *http.Request) {
+	err := signTemplate.Execute(w, r.FormValue("content"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+var signTemplate = template.Must(template.New("sign").Parse(signTemplateHTML))
+
+const signTemplateHTML = `
+	<html>
+		<body>
+			<p>Escribiste:</p>
+			<pre>{{.}}</pre>
+		</body>
+	</html>
+`
